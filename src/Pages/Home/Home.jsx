7b5062a0c5b18d2454ae43884c9e8ad77569e629
mesaaransh <mesaaransh@gmail.com>
2023-11-06 17:08:52 +0530

    navbar complete + thoda sa homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import "./Home.css"
+import Navbar from '../Components/Navbar/Navbar'
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar/>
+      <div className="home">
+        <div className="homeHeadings">
+          <h4 className='homeTagUp'>Sehat Ka Saathi</h4>
+          <h1 className='homeHead'>Nurses for Home</h1>
+          <p className='homeTagDown'>
+            24/7 Service, Private Consultation + Emergency Services <br /> 
+            Starts at just 10$, Exclusively on our mobile app
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
