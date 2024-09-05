bb02b4d21e044684305840bd2406a5e1bc449cdf
Pariansh Mahajan <116679976+ParianshMahajan@users.noreply.github.com>
2024-09-05 23:23:42 +0530

    implemented maps apis

--- server/Routers/userRouter.js
+++ server/Routers/userRouter.js
@@ -1,12 +1,13 @@
 const express=require('express');
 const { apply, sendData, createUser, updateProfile, pastApp, changeSlot, test, selectSlot } = require('../Controllers/UserFuncs');
+const { SearchLocation } = require('../Controllers/CommonFuncs');
 const app=express();
 const userRouter=express.Router();
 
 // not loggedIn then /newuser 
 userRouter
 .route('/test')
-.get(test)
+.get(SearchLocation)
 
 
 module.exports=userRouter;
