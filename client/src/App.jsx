31ab74bd7b3bf3c8fcb7073437735ed5fba257cc
dgbkn <anandrambkn@gmail.com>
2024-09-05 20:58:37 +0530

    asda

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          {/* GOod */}
+          {/* Good */}
           <Route path="/" element={<Home/> }></Route>
           <Route path="/search" element={<Search/> }></Route>
           <Route path="/nurse/:id" element={<NurseProfile/>}></Route>
