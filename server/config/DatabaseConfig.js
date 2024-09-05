1f1299efebbba974dfc1c0f4196f3296056fb682
dgbkn <anandrambkn@gmail.com>
2024-09-05 23:44:48 +0530

    done

--- server/config/DatabaseConfig.js
+++ server/config/DatabaseConfig.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
-const DB = process.env.DATABASE.replace("<username>", process.env.USER).replace(
+const DB = process.env.DATABASE.replace("<username>", process.env.USERM).replace(
   "<password>",
   process.env.PASSWORD
 );
 
+console.log(DB);
 
 mongoose
   .connect(DB)
